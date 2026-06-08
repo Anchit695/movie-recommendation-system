3a0aa6a49d69ec9af5eac45e86fd5255f0c6de9d
Anchit695 <anchitlakra03@gmail.com>
2026-06-08 18:52:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ similarity =pickle.load(open('similarity.pkl','rb'))
 st.title('Movie Recommendation System')
 
 selected_movie_name = st.selectbox(
-'How would you like to be contacted?',
+"select a movie",
 movies['title'].values)
 
 if st.button('Recommend'):
